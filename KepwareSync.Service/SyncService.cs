dbed70e63c33eb41e142c6f8531ac5b647b0c323
Ray Labbe <rlabbe@ptc.com>
2025-06-30 11:01:37 -0400

    Revert "fix(sync): consistencey with other async calls"
    This reverts commit 907dc3966b025f32942d0de446faf4e49e272524.

--- KepwareSync.Service/SyncService.cs
+++ KepwareSync.Service/SyncService.cs
@@ -283,20 +283,7 @@ namespace Kepware.SyncService
                 overwrite.Apply(project);
             }
 
-            if (await kepServerClient.TestConnectionAsync(cancellationToken).ConfigureAwait(false))
-            {
-                m_logger.LogInformation("Synchronizing project from {ProjectSource} to {ClientName}-kepserver ({ClientHostName})",
-                    projectSource, clientName, kepServerClient.ClientHostName);
-
-                // If the project is not loaded, we need to load it first
-                if (project.ProjectId == 0)
-                {
-                    var currentProject = await kepServerClient.Project.LoadProject(false, cancellationToken).ConfigureAwait(false);
-                    if (currentProject != null)
-                    {
-                        project.ProjectId = currentProject.ProjectId;
-                    }
-                }
+            if (await kepServerClient.TestConnectionAsync(cancellationToken))
             {
                 var (inserts, updates, deletes) = await kepServerClient.Project.CompareAndApply(project, cancellationToken).ConfigureAwait(false);
 
